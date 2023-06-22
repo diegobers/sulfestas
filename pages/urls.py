1153f187148f3ae3a65500bca8892ea814f662ae
Diego Bernardi <diegobernardisza@gmail.com>
2023-06-22 05:30:53 -0300

    update navbar && insert content pages.views.index

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from django.views.generic.base import TemplateView
+from . import views
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='pages/index.html'), name='index'),
+    path('', views.index, name='index'),
     #path('about', views.about, name='about'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from products.models import Product
+
+def index(request):
+    products = Product.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'products': products, 
+    }
+
+    return render(request, 'pages/index.html', context)
